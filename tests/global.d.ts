6330e51773744842315615745489d55cdbbcbd6e
Roger Qiu <roger.qiu@polyhack.io>
2022-09-12 13:19:45 +1000

    feat: introducing `Timer` as an object for tracking `setTimeout`, to be used for async deadlines

---
+++ tests/global.d.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-var */
+
+/// <reference types="jest-extended" />
+
+/**
+ * Follows the globals in jest.config.ts
+ * @module
+ */
+declare var projectDir: string;
+declare var testDir: string;
+declare var dataDir: string;
+declare var defaultTimeout: number;
+declare var maxTimeout: number;
---
+++ tests/utils.ts
@@ -0,0 +1,5 @@
+async function sleep(ms: number): Promise<void> {
+  return await new Promise<void>((r) => setTimeout(r, ms));
+}
+
+export { sleep };
